ad311a9a8d77978a53e73125f1f16976036d422e
LilySny <liviacastilholi@gmail.com>
2019-09-22 14:02:49 -0300

    will commit

--- lib/test3.dart
+++ lib/test3.dart
@@ -21,16 +21,31 @@ main() {
   will$.add("Hill");
   smith$.add("Smite");
 
-  Observable.merge([will$, smith$])..listen((i) => print("Test3 = name: $i")); // merge the both controllers and then print them
-  Observable.concat([will$, smith$]).where((v) => v.contains("ill"))//concats the ones who has "ill"
-  ..listen((i) => print("Test3 = concat:$i ")); //prints the result
+
+  Observable.merge([will$, smith$]) // merge the both controllers and then print them
+  .where((i) => i.toLowerCase().contains("sm"))
+  .listen((i) => print("Merge: $i")); // it will print each element of both controller separately
+  
+  Observable.concat([will$, smith$]) // it only emits the values of the second stream, when the previous streams terminate succefully
+  .where((v) => v.contains("ill"))//concats the ones who has "ill"
+  .listen((i) => print("Concat:$i")); // it only prints the will$(first stream), cause it never ends
 
   Observable.fromIterable(list).expand<String>((v) => [v]) //expands the elements from the list
-  ..listen((n) => print("expand names: $n")); //print each one separately
+  ..listen((n) => print("expand names: $n")); //print each one separately 
 
-  Observable.combineLatest2(will$, smith$, (w, s) => print("$w " + "$s")) //combines the values of each controller
-  ..listen(print);
+  Observable.combineLatest2(will$, smith$, (w, s) => // it really combines the values of both streams
+  print("CombineLatest: $w " + "$s")) //combines the values of each controller
+  .take(3)
+  ..listen((i) => i); 
 
-  Observable.zip2(will$, smith$, (w, s) => print("Smitation: $s"  + "$w"))// zips the both controllers (waits until get data)
+  Observable.zip2(will$, smith$, (w, s) => // zips the both streams into one observable
+  print("Smitation: $s"  + "$w"))
+  .take(2)
   ..listen((i) => i);
+
+  // resume:
+  // merge: gets each element of the merged streams
+  // concat: it only emits the values of the second stream, when the previous streams terminate succefully
+  // combineLatest: combine the streams values
+  // zip: zips the streams into one observable 
 }
